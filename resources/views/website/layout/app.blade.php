44445c2378f6562775c4698c7d633ce394595229
Mudassar Abbas <abbas8156@gmail.com>
2025-12-05 00:31:20 +0500

    Home page design

--- resources/views/website/layout/app.blade.php
+++ resources/views/website/layout/app.blade.php
@@ -18,7 +18,22 @@
         @yield('css')
     </head>
     <body>
-        
+        <div class="preloader">
+            <div class="loader">
+                <div class="ytp-spinner">
+                    <div class="ytp-spinner-container">
+                        <div class="ytp-spinner-rotator">
+                            <div class="ytp-spinner-left">
+                                <div class="ytp-spinner-circle"></div>
+                            </div>
+                            <div class="ytp-spinner-right">
+                                <div class="ytp-spinner-circle"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         @include('website.layout.partials.header')
 
         @yield('content')
